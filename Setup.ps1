17a3099d0bd9b35bc33634a6c4a6ac53fba4590b
Travis Plunk <travis.plunk@microsoft.com>
2016-03-16 22:11:34 -0700

    added reboot example setup and added setup for client sku

--- Setup.ps1
+++ Setup.ps1
@@ -1,9 +1,22 @@
+param(
+    [switch]
+    $reboot
+)
 [String] $moduleRoot = Split-Path -Parent $Script:MyInvocation.MyCommand.Path
 Register-PSRepository -Name xDscDiagnostics -SourceLocation https://ci.appveyor.com/nuget/xDscDiagnostics -ErrorAction SilentlyContinue
 Install-Module xDscDiagnostics -Repository xDscDiagnostics -force
 
+Get-NetAdapter | %{Set-NetConnectionProfile  -InterfaceAlias $_.Name -NetworkCategory Private}
+Set-WSManQuickConfig -Force
 
-configuration Example
+$name = "Example"
+If($reboot)
+{
+    del C:\WINDOWS\System32\Configuration\ConfigurationStatus\*
+    $name +="Reboot"
+}
+
+configuration $name
 {
   log example
   {
@@ -15,13 +28,29 @@ configuration Example
     TestScript =  { return $false}
     SetScript = {throw }
   }
+  if($reboot)
+  {
+    Script Reboot
+    {
+        GetScript = { return @{}}
+        TestScript =  { return (Test-path $env:SystemDrive\rebooted.txt)}
+        SetScript = {
+            $global:DSCMachineStatus = 1 
+            ''|Out-File $env:SystemDrive\rebooted.txt 
+        }
+    }      
+  }
   LocalConfigurationManager{
       ConfigurationMode = 'ApplyOnly'
+      RebootNodeIfNeeded = $true
   }
 }
 
-example
-Set-DscLocalConfigurationManager .\Example
-Start-DscConfiguration .\Example -Wait
+&$name
+Set-DscLocalConfigurationManager .\$name
+Start-DscConfiguration .\$name -Wait -force
 
-import-module "$moduleroot\modules\startdemo\startdemo.psd1" -force -Scope Global
+Remove-DscConfigurationDocument -Stage Pending
+Remove-DscConfigurationDocument -Stage Current
+
+$env:psmodulePath = "$env:psmodulePath;$moduleroot\modules"
