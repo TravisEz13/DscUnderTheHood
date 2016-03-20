ee9f0d73ae82148e14821333983bb438643a850a
Travis Plunk <tplunk@microsoft.com>
2016-03-19 22:23:09 -0700

    added initial encryption demo

---
+++ Encryption/demo.ps1
@@ -0,0 +1,8 @@
+get-content .\EncryptedAtRestConfiguration.psm1
+import-module .\EncryptedAtRestConfiguration.psm1 -force
+
+EncryptedAtRest
+
+start-dscconfiguration .\EncryptedAtRest -wait -verbose -force
+
+Get-content "$env:windir\system32\configuration\current.mof"
