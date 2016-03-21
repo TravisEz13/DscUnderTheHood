ca18d280dc7d96ac5126541d3dc176405b34b6bd
Travis Plunk <tplunk@microsoft.com>
2016-03-21 12:28:58 -0700

    Added init of encryption certificate

--- Init.ps1
+++ Init.ps1
@@ -6,3 +6,15 @@ Get-NetAdapter | %{Set-NetConnectionProfile  -InterfaceAlias $_.Name -NetworkCat
 Set-WSManQuickConfig -Force > $null
 
 $env:psmodulePath = "$env:psmodulePath;$moduleroot\modules"
+
+$cert = @(dir Cert:\LocalMachine\My).where{$_ |Test-Certificate -EKU @('1.3.6.1.4.1.311.80.1') -DNSName DscEncryptionCert -ErrorAction SilentlyContinue -WarningAction SilentlyContinue }
+if($cert.Count -ge 1)
+{
+    $global:DscEncryptionCert = $cert
+}
+else {
+    $cert = New-SelfSignedCertificate -Type DocumentEncryptionCert -KeyUsage @('KeyEncipherment', 'DataEncipherment') -DnsName 'DscEncryptionCert'                                                                                 
+    $cert |Export-Certificate -FilePath "$env:temp\DscPublicKey.cer"                                                                                                                                                               
+    Import-Certificate -FilePath "$env:temp\DscPublicKey.cer" -CertStoreLocation Cert:\LocalMachine\Root
+    $global:DscEncryptionCert = $cert
+}
