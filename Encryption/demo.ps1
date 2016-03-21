68b9f9c741b961465a48878980eb050bde09ebd2
Travis Plunk <tplunk@microsoft.com>
2016-03-21 16:16:59 -0700

    added credential encryption demo

--- Encryption/demo.ps1
+++ Encryption/demo.ps1
@@ -1,3 +1,29 @@
+# Credential encryption examples
+# review the configuration
+ise .\EncryptedCredential.psm1
+
+#import the configuration
+import-module .\EncryptedCredential.psm1 -force
+
+# Compile the meta configuration
+EncryptedCredentialMeta
+
+# Set the LCM to use the meta
+Set-DscLocalConfigurationManager -path .\EncryptedCredentialMeta
+
+# Compile the configuration
+EncryptedCredential -ConfigurationData (Get-EncryptedCredentialConfigurationData) -runAsCredential (Get-Credential)
+
+# Review the contents of the mof sent to LCM
+notepad ".\EncryptedCredential\localhost.mof"
+
+# Start the configuration
+start-dscconfiguration .\EncryptedCredential -wait -verbose -force 
+
+# Review the contents of the MOF the LCM stored
+Get-content "$env:windir\system32\configuration\current.mof"
+
+#EncryptedAtRest Example
 get-content .\EncryptedAtRestConfiguration.psm1
 import-module .\EncryptedAtRestConfiguration.psm1 -force
 
