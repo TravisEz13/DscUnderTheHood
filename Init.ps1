af3a4126844570f8244dd62ff83de0db386fe274
Travis Plunk <tplunk@microsoft.com>
2016-03-21 10:43:07 -0700

    suppressed error setting network profile

--- Init.ps1
+++ Init.ps1
@@ -2,7 +2,7 @@
 Register-PSRepository -Name xDscDiagnostics -SourceLocation https://ci.appveyor.com/nuget/xDscDiagnostics -ErrorAction SilentlyContinue
 Install-Module xDscDiagnostics -Repository xDscDiagnostics -force
 
-Get-NetAdapter | %{Set-NetConnectionProfile  -InterfaceAlias $_.Name -NetworkCategory Private}
+Get-NetAdapter | %{Set-NetConnectionProfile  -InterfaceAlias $_.Name -NetworkCategory Private -erroraction SilentlyContinue}
 Set-WSManQuickConfig -Force > $null
 
 $env:psmodulePath = "$env:psmodulePath;$moduleroot\modules"
