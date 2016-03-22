d4dfd57eab65d8df4dd2a7a60822f139b16d25f7
Travis Plunk <tplunk@microsoft.com>
2016-03-21 17:38:59 -0700

    Added per user non-admin example

--- Encryption/EncryptedCredential.psm1
+++ Encryption/EncryptedCredential.psm1
@@ -14,13 +14,20 @@ Configuration EncryptedCredential
         [PSCredential]
         $runAsCredential 
     )
+
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration    
+    Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
+    
     node localhost
     {
+        # Script resource running as LCM
+        # not using credentials
         Script DontRunAs
         {
             GetScript = {
+                # LCM Username
                 @{
-                    LcmUsername = ([System.Security.Principal.WindowsIdentity]::GetCurrent().Name)
+                    Result = ([System.Security.Principal.WindowsIdentity]::GetCurrent().Name)
                 }
             }
             TestScript = {
@@ -34,8 +41,9 @@ Configuration EncryptedCredential
         Script RunAs
         {
             GetScript = {
+                # RunAs Username
                 @{
-                    username = ([System.Security.Principal.WindowsIdentity]::GetCurrent().Name)
+                    Result = ([System.Security.Principal.WindowsIdentity]::GetCurrent().Name)
                 }
             }
             TestScript = {
@@ -45,6 +53,18 @@ Configuration EncryptedCredential
             SetScript = {
                         #
                 }
+            PsDscRunAsCredential = $runAsCredential
+        }
+        xPackage VsCode
+        { 
+            Name =  'VsCode'
+            Path = (Resolve-uri -uri 'https://go.microsoft.com/fwlink/?LinkID=623230') 
+            ProductId = ''
+            Arguments = '-s'
+            InstalledCheckRegHive = 'CurrentUser' 
+            InstalledCheckRegKey = 'Software\Microsoft\Windows\CurrentVersion\Uninstall\Code' 
+            InstalledCheckRegValueName = 'DisplayName'
+            InstalledCheckRegValueData = 'Code' 
             PsDscRunAsCredential = $runAsCredential
         }
         
@@ -59,7 +79,45 @@ function Get-EncryptedCredentialConfigurationData
                     NodeName = 'localhost'
                     #CertificateFile = "$env:temp\DscPublicKey.cer"
                     CertificateId =  $global:DscEncryptionCert.Thumbprint
+                    PSDscAllowDomainUser = $true
                 }    
             )
         }
+}
+
+function Resolve-Uri
+{
+    [CmdletBinding()]
+    param (
+        [Uri] $uri
+    )
+
+    $response = $null
+    try
+    {  
+        $response = Invoke-WebRequest -uri $uri -UseBasicParsing -MaximumRedirection 0 -Method Head -ErrorAction ignore
+    }
+    catch
+    {
+        try
+        {
+            $response = Invoke-WebRequest -uri $uri -UseBasicParsing -MaximumRedirection 0 -ErrorAction Ignore -Method Get
+        }
+        catch
+        {
+            return $null
+        }
+    }
+    if($response.StatusCode -eq 200)
+    {
+        return $uri
+    }
+    elseif($response.StatusCode -eq 302)
+    {
+        Resolve-Uri -uri $response.Headers.Location
+    }
+    else
+    {
+        throw "Could not resolve uri: $($uri.AbsoluteUri)"
+    }
 }
