90617025c1c938e9c527c816dad50c654f98ca3d
Travis Plunk <tplunk@microsoft.com>
2016-03-21 17:40:10 -0700

    removed dead code comments

--- Init.ps1
+++ Init.ps1
@@ -13,15 +13,11 @@ if($cert.Count -ge 1)
     $global:DscEncryptionCert = $cert
 }
 else {
-    <#$rootCert = New-SelfSignedCertificate  -Type Custom -DnsName 'DscEncryptionSelfSignedCa' -CertStoreLocation Cert:\LocalMachine\my -KeyUsage CertSign -Provider "Microsoft RSA SChannel Cryptographic Provider"
-    $cert = New-SelfSignedCertificate -Type DocumentEncryptionCert -DnsName 'DscEncryptionCert' -Signer $rootCert#>
     $cert = New-SelfSignedCertificate -Type DocumentEncryptionCert -DnsName 'DscEncryptionCert' -Provider "Microsoft RSA SChannel Cryptographic Provider"
     $cert | Export-Certificate -FilePath "$env:temp\DscPublicKey.cer"  -Force                                                              
-    #$rootCert |Export-Certificate -FilePath "$env:temp\DscCaPublicKey.cer"  -Force                                                                                                                                                             
     Import-Certificate -FilePath "$env:temp\DscPublicKey.cer" -CertStoreLocation Cert:\LocalMachine\Root > $null
     $global:DscEncryptionCert = $cert
     
-    <#if(!($cert|Test-Certificate -EKU @('1.3.6.1.4.1.311.80.1') -DNSName DscEncryptionCert))#>
     if(!($cert.Verify()))
     {
         throw 'certificate setup failed'
